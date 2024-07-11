514135c4c847b7f17e6f54028180aa9fc2e7a807
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-07-11 09:38:50 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VisualizeComponent } from './visualize/visualize.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
+    { path: 'visualize', component: VisualizeComponent},
+];
---
+++ src/app/visualize/visualize.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualize',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualize.component.html',
+  styleUrl: './visualize.component.css'
+})
+export class VisualizeComponent {
+  
+}
